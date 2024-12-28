d74f9581a5889c0deabdf7521becf9d243a394f4
mahbeer2012 <mahbeermubashir031@gmali.com>
2024-12-28 01:42:36 -0800

    activity 1

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("welcome to the world of programming")
+
+#print a number 
+print(4)
+
+# next line after print
+print("hello python /n")
+
+#pass multiple arguments for painting
+print("hello" ,5)
+
+# end argument of print statement 
+print("welcome to " , end= "*")
